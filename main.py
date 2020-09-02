7edb752798951c5b6e81977980ac1a9e925fa0fa
safwanvk <safwanvalakundil@gmail.com>
2020-09-02 13:58:48 +0530

    :zap: Set model BlogPost

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from datetime import datetime
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite3///posts.db'
+db = SQLAlchemy(app)
+
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=True)
+    content = db.Column(db.Text, nullable=True)
+    author = db.Column(db.String(20), nullable=True, default='N/A')
+    date_published = db.Column(db.DateTime, nullable=True, default=datetime.utcnow())
+
+    def __repr__(self):
+        return 'Blog Post' + str(self.id)
+
 
 all_posts = [
     {'title': 'Post 1',
