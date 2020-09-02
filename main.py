447bc17ea9cff5e237bcaca22ed1cfa5e65cbc63
safwanvk <safwanvalakundil@gmail.com>
2020-09-02 12:37:47 +0530

    :zap: basic

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+all_posts = [
+    {'title': 'Post 1',
+     'content': 'This is the content of Post 1',
+     'author': 'Ajas'},
+    {'title': 'Post 2',
+     'content': 'This is the content of Post 2'}
+]
 
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', context=all_posts)
 
 
 if __name__ == "__main__":
