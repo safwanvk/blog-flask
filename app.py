05cda1eb7a08f4ec0ff7b0d0392061dcb203d7db
safwanvk <safwanvalakundil@gmail.com>
2020-09-02 16:24:54 +0530

    :zap: Set Delete Post

--- app.py
+++ app.py
@@ -40,5 +40,13 @@ def posts():
         return render_template('posts.html', context=all_posts)
 
 
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
